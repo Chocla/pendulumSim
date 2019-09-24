155734a7cb5024eb70d4286acca2bf81582c2193
Chocla <joe.mcdonough@live.com>
2019-09-24 11:52:17 -0700

    general repo cleanup

--- cart.py
+++ cart.py
@@ -3,68 +3,88 @@ import control
 import pygame
 
 g = -10
-
+#TODO: (Low Priority) Make it so that user can grab and move the pendulum
 class cart():
 
-    def __init__(self, state, m=1., M=5., L=50.,controlled=False):
+    def __init__(self, state, m=1., M=5., L=25.,controlled=False):
         self.state = state
         self.m = m
         self.M = M
         self.L = L
+        self.d = 1.
         self.x = state[0]
         self.th = state[2]
-        self.cw = 45*np.sqrt(M / 5) #cart width
-        self.ch = 0.5 * self.cw #cart height
+
+        # Cart Data
+        self.cw = 50 #cart width
+        self.ch = 25 #cart height
         self.cx = self.x + (self.cw / 2)
         self.cy = self.ch / 2
-        self.px = self.cx + self.L * np.sin(self.th)
-        self.py = self.cy - self.L * np.cos(self.th)
-        self.pr = 10*np.sqrt(self.m)
-        self.dt = .16
-        self.d = 1.
-        self.eigs = np.array([-1.1,-1.2,-1.3,-1.4])
-        # self.eigs = np.array([-2.1,-2.2,-2.3,-2.4])
 
+        # Pendulum Data
+        self.px = self.cx + 50 * np.sin(self.th)
+        self.py = self.cy - 50 * np.cos(self.th)
+        self.pr = 10
+    
+        self.dt = .16
+
+        self.eigs = np.array([-1.1,-1.2,-1.3,-1.4])
+        # self.K = getOptimalK(self.m,self.M,self.L,g,self.d) if controlled else np.zeros(4)
         self.K = calculateK(self.m,self.M,self.L,g,self.d,self.eigs) if controlled else np.zeros(4)
-        # print(self.K) #Expected: [-2.4,-8.75, 158.3,65.5]
-        
 
     def draw(self,screen):
+        """Draw cart-pendulum on pygame screen"""
         w,h = screen.get_size()
         center = w/2
-        # print(self.cx)
         pygame.draw.rect(screen, (255,0,0), (center + self.x, h/2 - self.ch, self.cw,self.ch))
         pygame.draw.line(screen, (255,0,0), (center + self.cx,h/2-self.cy),(center + self.px,h/2-self.py), 3 )
-    
         pygame.draw.circle(screen, (255,0,0), (int(center + self.px),h/2 - int(self.py)), int(self.pr))
 
-    
     def update(self):
-        self.state = heuns_method_cart(self.dt,self.state,self.m,self.M,self.L,g,self.d,
+        """Updates the internal state of the cart-pendulum system"""
+        self.state = heuns(self.dt,self.state,self.m,self.M,self.L,g,self.d,
         -1*np.dot(self.K,(self.state - np.array([0,0,np.pi,0])))) 
-        # 0)
         self.x = self.state[0]
         self.th = self.state[2]
         self.cx = self.x + (self.cw / 2)
-        self.px = self.cx + self.L * np.sin(self.th)
-        self.py = self.cy - self.L * np.cos(self.th)
-        
+        self.px = self.cx + 50 * np.sin(self.th)
+        self.py = self.cy - 50 * np.cos(self.th)
+
+#FIXME: Understand how LQR actually works and implement it
+def getOptimalK(m,M,L,g,d):
+        s = 1
+        A = np.array([[0,1,0,0],[0,-d/M, -m*g/M,0],[0,0,0,1],[0,-s*d/(M*L), -s*(m+M)*g/(M*L),0]])
+        B = [[0],[1/M],[0],[s/(M*L)]]
+        Q = np.array([[1,0,0,0],[0,1,0,0],[0,0,10,0],[0,0,0,100]])
+        R = 0.0001
+        K,S,E = control.lqr(A,B,Q,R)
+        print(K)
+        print(np.linalg.eig(A-B*K)[0])
+        return K
+
+
 def calculateK(m,M,L,g,d,eigs): 
+    """calculate that places eigenvalues for A-B*K"""
+     #TODO: Figure out how this works and implement it myself
     s = 1
     A = np.array([[0,1,0,0],[0,-d/M, -m*g/M,0],[0,0,0,1],[0,-s*d/(M*L), -s*(m+M)*g/(M*L),0]])
     B = [[0],[1/M],[0],[s/(M*L)]]
 
-    K = control.place(A,B,eigs) #TODO: Figure out how this works and implement it myself
-    print(np.linalg.eig(A-np.outer(B,K)))
+    K = control.place(A,B,eigs)
+    
     return K
 
-def heuns_method_cart(dt, state,m,M,L,g,d,u):
-    ye = state + dt*pendulum_dy(state,m,M,L,g,d,u)
-    return state + (dt/2)*(pendulum_dy(state,m,M,L,g,d,u) + pendulum_dy(ye,m,M,L,g,d,u))
-# Shitty Solver, implement something better?? 
-def eulers_method_cart(dt, state,m,M,L,g,d,u):
-    return state + dt*pendulum_dy(state,m,M,L,g,d,u)
-def pendulum_dy(state,m,M,L,g,d,u):
+def heuns(dt, state,m,M,L,g,d,u):
+    """Numerical ode solver using heun's method.
+    returns updated state after one time step
+    """
+    ye = state + dt*stateDiff(state,m,M,L,g,d,u)
+    return state + (dt/2)*(stateDiff(state,m,M,L,g,d,u) + stateDiff(ye,m,M,L,g,d,u))
+
+def stateDiff(state,m,M,L,g,d,u):
+    """ Calculate the differential for inverted pendulum on a cart given a state
+    Returns as numpy array
+    """
     dy = np.zeros(4,dtype=np.float64)
     Sy = np.sin(state[2])
     Cy = np.cos(state[2])
@@ -79,9 +99,8 @@ def pendulum_dy(state,m,M,L,g,d,u):
 
 if __name__ == "__main__":
     m, M, L, g, d = 1., 5., 2., -10., 1.
-    K = calculateK(m,M,L,g,d,[-1.1,-1.2,-1.3,-1.4])
-    print(K)
-    print(np.dot(-K,[1,1,1,1]))
+    K = getOptimalK(m,M,L,g,d)
+
     state = np.array([0,1,2,3])
     # print(np.outer(state,state) )
     # print("hello")
--- main.py
+++ main.py
@@ -4,11 +4,9 @@ import numpy as np
 import control
 (width, height ) = (600,300)
 screen = pygame.display.set_mode((width,height))
-
-c = cart.cart(np.array([0,0,np.pi+.1,0],dtype=np.float64),controlled=False)
-
 pygame.display.set_caption('Pendulum Simulation')
 
+c = cart.cart(np.array([0,0,np.pi + .1,0],dtype=np.float64),controlled=False)
 
 pygame.display.flip()
 clock = pygame.time.Clock()
@@ -17,7 +15,6 @@ fps = 60
 while running:
     clock.tick(fps)
     c.update()
-
     c.draw(screen)
     pygame.display.flip()
     for event in pygame.event.get():
