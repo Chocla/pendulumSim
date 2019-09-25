045536b89828cf13d8102e0f6686051f299fc52d
Chocla <joe.mcdonough@live.com>
2019-09-25 15:17:58 -0700

    Imlemented LQR, the function itself works but using the K matrix doesn't

--- cart.py
+++ cart.py
@@ -1,12 +1,15 @@
 import numpy as np 
 import control
 import pygame
+import scipy.linalg
 
 g = -10
+
+
 #TODO: (Low Priority) Make it so that user can grab and move the pendulum
 class cart():
 
-    def __init__(self, state, m=1., M=5., L=25.,controlled=False):
+    def __init__(self, state, m=1., M=5., L=2.,controlled=False):
         self.state = state
         self.m = m
         self.M = M
@@ -29,49 +32,56 @@ class cart():
         self.dt = .16
 
         self.eigs = np.array([-1.1,-1.2,-1.3,-1.4])
-        # self.K = getOptimalK(self.m,self.M,self.L,g,self.d) if controlled else np.zeros(4)
-        self.K = calculateK(self.m,self.M,self.L,g,self.d,self.eigs) if controlled else np.zeros(4)
+        self.K = getOptimalK(self.m,self.M,self.L,g,self.d) if controlled else np.zeros(4)
+        # self.K = calculateK(self.m,self.M,self.L,g,self.d,self.eigs) if controlled else np.zeros(4)
 
     def draw(self,screen):
         """Draw cart-pendulum on pygame screen"""
         w,h = screen.get_size()
         center = w/2
+
         pygame.draw.rect(screen, (255,0,0), (center + self.x, h/2 - self.ch, self.cw,self.ch))
         pygame.draw.line(screen, (255,0,0), (center + self.cx,h/2-self.cy),(center + self.px,h/2-self.py), 3 )
         pygame.draw.circle(screen, (255,0,0), (int(center + self.px),h/2 - int(self.py)), int(self.pr))
-
+  
     def update(self):
         """Updates the internal state of the cart-pendulum system"""
-        self.state = heuns(self.dt,self.state,self.m,self.M,self.L,g,self.d,
-        -1*np.dot(self.K,(self.state - np.array([0,0,np.pi,0])))) 
+        self.state = rk4(self.dt,self.state,self.m,self.M,self.L,g,self.d,
+        np.dot(-self.K,(self.state - np.array([0,0,np.pi,0])))) 
         self.x = self.state[0]
         self.th = self.state[2]
         self.cx = self.x + (self.cw / 2)
         self.px = self.cx + 50 * np.sin(self.th)
         self.py = self.cy - 50 * np.cos(self.th)
 
-#FIXME: Understand how LQR actually works and implement it
+#TODO: Understand how LQR actually works and implement it
 def getOptimalK(m,M,L,g,d):
-        s = 1
-        A = np.array([[0,1,0,0],[0,-d/M, -m*g/M,0],[0,0,0,1],[0,-s*d/(M*L), -s*(m+M)*g/(M*L),0]])
-        B = [[0],[1/M],[0],[s/(M*L)]]
-        Q = np.array([[1,0,0,0],[0,1,0,0],[0,0,10,0],[0,0,0,100]])
-        R = 0.0001
-        K,S,E = control.lqr(A,B,Q,R)
-        print(K)
-        print(np.linalg.eig(A-B*K)[0])
-        return K
+    """Find optimal K matrix using LQR"""
+    s = 1.
+    A = np.matrix([[0,1,0,0],[0,-d/M, -m*g/M,0],[0,0,0,1],[0,-s*d/(M*L), -s*(m+M)*g/(M*L),0]])
+
+    B = np.matrix([[0],[1/M],[0],[s/(M*L)]])
+
+    Q = np.matrix([[.01,0,0,0],[0,1,0,0],[0,0,10,0],[0,0,0,100]],dtype=np.float64)
+
+    R = np.matrix(0.001)
+
+    tmp = scipy.linalg.solve_continuous_are(A,B,Q,R)
+    K = scipy.linalg.inv(R)*B.T*tmp
+    print(K)
+    #FIXME: This is producing the correct eigenvalues, but plugging it into the control system causes catastrophic overflow problems
+    print("Eigs:", np.linalg.eig(A-B*K)[0])
+    return K
 
 
 def calculateK(m,M,L,g,d,eigs): 
-    """calculate that places eigenvalues for A-B*K"""
-     #TODO: Figure out how this works and implement it myself
+    """calculate K that places eigenvalues for A-B*K"""
     s = 1
     A = np.array([[0,1,0,0],[0,-d/M, -m*g/M,0],[0,0,0,1],[0,-s*d/(M*L), -s*(m+M)*g/(M*L),0]])
     B = [[0],[1/M],[0],[s/(M*L)]]
 
     K = control.place(A,B,eigs)
-    
+    print(K)
     return K
 
 def heuns(dt, state,m,M,L,g,d,u):
@@ -80,6 +90,13 @@ def heuns(dt, state,m,M,L,g,d,u):
     """
     ye = state + dt*stateDiff(state,m,M,L,g,d,u)
     return state + (dt/2)*(stateDiff(state,m,M,L,g,d,u) + stateDiff(ye,m,M,L,g,d,u))
+
+def rk4(dt, state, m,M,L,g,d,u):
+    k1 = dt*stateDiff(state,m,M,L,g,d,u)
+    k2 = dt*stateDiff(state+(k1/2),m,M,L,g,d,u)
+    k3 = dt*stateDiff(state+(k2/2),m,M,L,g,d,u)
+    k4 = dt*stateDiff(state+k3, m,M,L,g,d,u)
+    return state + (k1 + 2*k2 + 2*k3 + k4)/6
 
 def stateDiff(state,m,M,L,g,d,u):
     """ Calculate the differential for inverted pendulum on a cart given a state
@@ -91,9 +108,9 @@ def stateDiff(state,m,M,L,g,d,u):
     D = m*L*L*(M+m*(1-Cy**2))
 
     dy[0] = state[1]
-    dy[1] = (1/D)*(-(m**2)*(L**2)*g*Cy*Sy + m*(L**2)*(m*L*state[3]**2*Sy - d*state[1])) + u*m*L*L*(1/D)
+    dy[1] = (1/D)*(-(m**2)*(L**2)*g*Cy*Sy + m*(L**2)*(m*L*state[3]**2*Sy - d*state[1])) + m*L*L*(1/D)*u
     dy[2] = state[3]
-    dy[3] = (1/D)*((m+M)*m*g*L*Sy - m*L*Cy*(m*L*(state[3]**2)*Sy - d*state[1])) - u*m*L*Cy*(1/D)
+    dy[3] = (1/D)*((m+M)*m*g*L*Sy - m*L*Cy*(m*L*(state[3]**2)*Sy - d*state[1])) - m*L*Cy*(1/D)*u
 
     return dy
 
@@ -102,5 +119,3 @@ if __name__ == "__main__":
     K = getOptimalK(m,M,L,g,d)
 
     state = np.array([0,1,2,3])
-    # print(np.outer(state,state) )
-    # print("hello")
--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import pygame
 import cart
 import numpy as np
 import control
+
 (width, height ) = (600,300)
 screen = pygame.display.set_mode((width,height))
 pygame.display.set_caption('Pendulum Simulation')
 
-c = cart.cart(np.array([0,0,np.pi + .1,0],dtype=np.float64),controlled=False)
+c = cart.cart(np.array([-3,0,np.pi + .1,0],dtype=np.float64),controlled=False)
 
 pygame.display.flip()
 clock = pygame.time.Clock()
@@ -16,6 +17,7 @@ while running:
     clock.tick(fps)
     c.update()
     c.draw(screen)
+    # print(c.state)
     pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
